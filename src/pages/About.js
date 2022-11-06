03fb1d4d624cdbd2611cb9e9b9477cd6ddce74ed
Stas Masevych <109438310+StasMasevych@users.noreply.github.com>
2022-11-06 17:37:56 +0200

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae laborum dolorum atque sint vero consectetur assumenda a facilis eius accusantium minus hic, id, reprehenderit deserunt exercitationem dolor voluptatibus perspiciatis est Lorem ipsum dolor sit, amet consectetur adipisicing elit. In praesentium laboriosam facere delectus, ullam qui nobis ipsam? Error expedita cumque velit, amet iusto odio autem iste culpa accusamus, illum quia.</p>
+    </section>
+  )
+}
+
+export default About
